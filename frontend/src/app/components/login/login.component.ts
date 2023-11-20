22d9fcc6712a1a68fc4a2fa120a7bdfc21212e5a
petreastefann <petreastefann@gmail.com>
2023-11-20 18:55:57 +0200

    added test methods for checking the data from backend

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TestServiceService } from 'src/app/services/test-service.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  private static apiUrl = 'http://localhost:8080/';
+  public title = "title";
+  public securedTitle = "securedTitle";
 
+  constructor(private testService: TestServiceService) { }
+
+  ngOnInit(): void {
+    this.testService.getMessage().subscribe(data => {
+      this.title = data;
+    });
+    this.testService.getSecuredMessage().subscribe(data => {
+      this.securedTitle = data;
+    });
+  }
 }
