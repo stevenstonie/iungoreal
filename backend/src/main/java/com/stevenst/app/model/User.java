13ffc59cd59dfaa5e18325da6971b074739b366f
petreastefann <petreastefann@gmail.com>
2023-11-24 18:52:50 +0200

    updated User entity to implement spring security's UserDetails

--- backend/src/main/java/com/stevenst/app/model/User.java
+++ backend/src/main/java/com/stevenst/app/model/User.java
@@ -1,63 +1,74 @@
 package com.stevenst.app.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 
+	@Getter
 	@Column(nullable = false, unique = true)
 	private String username;
 
+	@Getter
 	@Column(nullable = false, unique = true)
 	private String email;
 
+	@Getter
 	@Column(nullable = false)
 	private String password;
 
-	public User(int id, String username, String password, String email) {
-		this.id = id;
-		this.username = username;
-		this.password = password;
-		this.email = email;
+	@Enumerated(EnumType.STRING)
+	private Role role;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority(role.name()));
 	}
 
-	public int getId() {
-		return this.id;
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
 	}
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
-	public String getUsername() {
-		return this.username;
-	}
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
-	public String getPassword() {
-		return this.password;
-	}
-
 	public void setPassword(String password) {
 		this.password = password;
-	}
-
-	public String getEmail() {
-		return this.email;
 	}
 
 	public void setEmail(String email) {
