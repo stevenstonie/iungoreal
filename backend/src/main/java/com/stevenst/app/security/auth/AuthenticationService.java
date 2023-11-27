d2ee4abf7272d86639e16158a25f0a40141b4b90
petreastefann <petreastefann@gmail.com>
2023-11-27 19:27:41 +0200

    added the service required for authentication

---
+++ backend/src/main/java/com/stevenst/app/security/auth/AuthenticationService.java
@@ -0,0 +1,48 @@
+package com.stevenst.app.security.auth;
+
+import com.stevenst.app.config.JwtService;
+import com.stevenst.app.model.Role;
+import com.stevenst.app.model.User;
+import com.stevenst.app.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+	private final UserRepository userRepo;
+	private final PasswordEncoder passwordEncoder;
+	private final JwtService jwtService;
+	private final AuthenticationManager authenticationManager;
+
+	public AuthenticationResponse register(RegisterRequest request) {
+		var user = User.builder()
+				.firstname(request.getFirstName())
+				.lastname(request.getLastName())
+				.email(request.getEmail())
+				.password(passwordEncoder.encode(request.getPassword()))
+				.role(Role.USER)
+				.build();
+				
+		userRepo.save(user);
+		var jwtToken = jwtService.generateToken(user);
+		return AuthenticationResponse.builder()
+				.token(jwtToken)
+				.build();
+	}
+
+	public AuthenticationResponse authenticate(AuthenticationRequest request) {
+		authenticationManager
+				.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+		var user = userRepo.findByEmail(request.getEmail())
+				.orElseThrow();
+		var jwtToken = jwtService.generateToken(user);
+		return AuthenticationResponse.builder()
+				.token(jwtToken)
+				.build();
+	}
+}
