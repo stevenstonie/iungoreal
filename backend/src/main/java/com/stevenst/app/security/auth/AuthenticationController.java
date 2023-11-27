bb14b8e63a9e1ac6b06848a96d9cf2f46da1319b
petreastefann <petreastefann@gmail.com>
2023-11-27 17:12:13 +0200

    added the security package and classes

---
+++ backend/src/main/java/com/stevenst/app/security/auth/AuthenticationController.java
@@ -0,0 +1,24 @@
+package com.stevenst.app.security.auth;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+	@PostMapping("/register")
+	public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+
+	}
+
+	@PostMapping("/authenticate")
+	public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest request) {
+		
+	}
+}
