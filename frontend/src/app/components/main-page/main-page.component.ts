6947bff3e52b6f2ea72be1cdf72bb8a82a8e9ff8
petreastefann <petreastefann@gmail.com>
2023-12-03 22:36:13 +0200

    added leaflet.js and a map for testing its display

--- frontend/src/app/components/main-page/main-page.component.ts
+++ frontend/src/app/components/main-page/main-page.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
   email: string;
+  declare map: L.Map;
 
   toggleMap() {
     console.log('toggle map');
   }
 
-  constructor() { 
+  constructor() {
     this.email = localStorage.getItem('email') ?? '';
+
   }
+
+  ngOnInit() {
+    this.map = L.map('map').setView([45.65, 25.603], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+     attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+   }   
+
 }
