0d82f0835969bf36f0250ce4c5e38ad77b0e8308
petreastefann <petreastefann@gmail.com>
2023-12-10 17:34:59 +0200

    added a map service to send and receive marker data from the map microservice

---
+++ project/frontend/src/app/services/map.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Marker } from '../models/marker';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MapService {
+  constructor(private httpClient: HttpClient) { }
+
+  getMarkers() {
+    return this.httpClient.get('http://localhost:8081/api/markers');
+  }
+
+  addMarker(marker: Marker) {
+    return this.httpClient.post('http://localhost:8081/api/addMarker', marker);
+  }
+
+  updateMarker(marker: Marker) {
+    return this.httpClient.put('http://localhost:8081/api/updateMarker', marker);
+  }
+
+  deleteMarker(id: number) {
+    return this.httpClient.delete(`http://localhost:8081/api/deleteMarker/${id}`);
+  }
+}
