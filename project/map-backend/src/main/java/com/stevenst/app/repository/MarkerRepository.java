2a45af26dce28eb40ec4b3d1b82094447b74f91c
petreastefann <petreastefann@gmail.com>
2023-12-10 18:35:59 +0200

    added the marker model and repo (bk)

---
+++ project/map-backend/src/main/java/com/stevenst/app/repository/MarkerRepository.java
@@ -0,0 +1,8 @@
+package com.stevenst.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.stevenst.app.model.Marker;
+
+public interface MarkerRepository extends JpaRepository<Marker, Integer> {
+	
+}
