b74c0f31080b3dbe74f8c16c2813a092750820ab
petreastefann <petreastefann@gmail.com>
2023-12-13 17:51:12 +0200

    added a dependency for testing and some methods for SecurityConfigurationTest.java

---
+++ project/app-backend/src/test/java/com/stevenst/app/config/SecurityConfigurationTest.java
@@ -0,0 +1,46 @@
+package com.stevenst.app.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SecurityConfigurationTest {
+	/*
+	* integration tests come after so finish this later. (+ change the class name)
+	*/
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	void whenGetNonWhitelistedEndpoint_then401() throws Exception {
+		mvc.perform(get("/api/user/currentUser"))
+				.andExpect(status().isUnauthorized());
+	}
+
+	@Test
+	void whenOptionsCorsRequest_then200() throws Exception {
+		mvc.perform(options("/api/user/currentUser")
+				.header("Access-Control-Request-Headers", "Authorization")
+				.header("Access-Control-Request-Method", "GET")
+				.header("Origin", "http://localhost:4200"))
+				.andExpect(status().isOk());
+	}
+
+	// @Test
+	// void whenPostLogin_then200() throws Exception {
+	// 	String jsonPayload = "{\"username\":\"user1\",\"password\":\"password\"}";
+
+	// 	mvc.perform(post("/api/auth/login")
+	// 			.contentType(MediaType.APPLICATION_JSON)
+	// 			.content(jsonPayload))
+	// 			.andExpect(status().isOk());
+	// }
+}
--- project/app-backend/src/test/java/com/stevenst/app/service/impl/JwtServiceImplTest.java
+++ project/app-backend/src/test/java/com/stevenst/app/service/impl/JwtServiceImplTest.java
@@ -5,9 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class JwtServiceImplTest {
-	/* TODO: need to figure out how to test this
-	currently cannot be tested as the JwtServiceImpl uses the config file for the secret key and when 
-	calling methods which use the secretKey (which will be null) the test will return an exception
+	/* TODO: need to figure out how to test this class
+	 * currently cannot be done as the JwtServiceImpl uses the config file for the secret key and 
+	 * a lot of methods use 'secretKey' which is null.
 	 */
 	@Test
 	void test() {
