aef3e536ea24bcb64a10b9d0ef43a16a302499fd
petreastefann <petreastefann@gmail.com>
2023-12-14 23:44:05 +0200

    added an ApiExceptionFormat file and changed the GlobalExceptionHandler

--- project/app-backend/src/main/java/com/stevenst/app/exception/GlobalExceptionHandler.java
+++ project/app-backend/src/main/java/com/stevenst/app/exception/GlobalExceptionHandler.java
@@ -1,15 +1,30 @@
 package com.stevenst.app.exception;
 
+import java.time.LocalDateTime;
+
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("An error occurred while processing the request.");
+    public ApiExceptionFormat handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        return buildResponseEntity(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    // -------------------------------------------------------------------
+
+    private ApiExceptionFormat buildResponseEntity(HttpStatus status, String message) {
+        ApiExceptionFormat apiException = new ApiExceptionFormat();
+        apiException.setStatus(status.value());
+        apiException.setMessage(message);
+        apiException.setTimestamp(LocalDateTime.now());
+        return apiException;
     }
 }
