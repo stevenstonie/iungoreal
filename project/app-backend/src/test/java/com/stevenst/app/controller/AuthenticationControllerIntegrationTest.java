b7c19edc5c52d9719202c022b493bbd68af01a1a
petreastefann <petreastefann@gmail.com>
2023-12-14 20:08:17 +0200

    added more test methods for authentication controller

--- project/app-backend/src/test/java/com/stevenst/app/controller/AuthenticationControllerIntegrationTest.java
+++ project/app-backend/src/test/java/com/stevenst/app/controller/AuthenticationControllerIntegrationTest.java
@@ -3,6 +3,13 @@ package com.stevenst.app.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stevenst.app.auth.AuthenticationRequest;
 import com.stevenst.app.auth.RegisterRequest;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.ServletException;
 import jakarta.transaction.Transactional;
 import org.h2.tools.Server;
 import org.junit.jupiter.api.AfterAll;
@@ -13,13 +20,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -75,6 +83,8 @@ class AuthenticationControllerIntegrationTest {
                 .andExpect(jsonPath("$.token").isNotEmpty());
     }
 
+    // TODO: check if these tests dont belong in AuthenticationServiceTest.java
+
     @Test
     @Transactional
     void testAuthenticationEndpointWithInvalidCredentials() throws Exception {
@@ -84,7 +94,55 @@ class AuthenticationControllerIntegrationTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(authenticationRequest)))
                 .andExpect(status().isUnauthorized());
-                // TODO: add another assertion for a thrown exception
+        // TODO: add another assertion for a thrown exception
+    }
+
+    @Test
+    @Transactional
+    void testRegistrationEndpointWithExistingEmail() throws Exception {
+        RegisterRequest registerRequest = new RegisterRequest(EMAIL, PASSWORD, "test", "user");
+
+        mockMvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(registerRequest)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @Transactional
+    void testRegistrationEndpointWithMissingCredentials() throws Exception {
+        RegisterRequest registerRequest = new RegisterRequest("", "", "", "");
+
+        assertThrows(ServletException.class,
+                () -> mockMvc.perform(post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(registerRequest))));
+    }
+
+    @Test
+    @Transactional
+    void testAuthenticationEndpointWithMissingCredentials() throws Exception {
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest("", "");
+
+        mockMvc.perform(post("/api/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(authenticationRequest)))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @Transactional
+    void testAuthenticationWithABadSignature() throws Exception {
+        String expiredToken = generateTokenWithBadSignature();
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest(EMAIL, PASSWORD);
+
+        assertThrows(SignatureException.class, () -> {
+            mockMvc.perform(post("/api/auth/login")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(authenticationRequest))
+                    .header("Authorization", "Bearer " + expiredToken))
+                    .andExpect(status().isUnauthorized());
+        });
     }
 
     // ------------------------------------------------------------------------
@@ -94,8 +152,20 @@ class AuthenticationControllerIntegrationTest {
 
         mockMvc.perform(post("/api/auth/register")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(registerRequest)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.token").isNotEmpty());
+                .content(objectMapper.writeValueAsString(registerRequest)));
+    }
+
+    public String generateTokenWithBadSignature() {
+        return Jwts
+                .builder()
+                .setClaims(new HashMap<>())
+                .setSubject(EMAIL)
+                .setIssuedAt(new Date(System.currentTimeMillis() - 1000))
+                .setExpiration(new Date(System.currentTimeMillis() - 10))
+                .signWith(
+                        Keys.hmacShaKeyFor(Decoders.BASE64
+                                .decode("1234abcd1234abcd1234abcd1234abcd1234abcd1234abcd1234abcd1234abcd")),
+                        SignatureAlgorithm.HS256)
+                .compact();
     }
 }
