9568425dd219c67961faaeac68cd86b6ee35e3e1
petreastefann <petreastefann@gmail.com>
2023-12-19 23:11:30 +0200

    (bk) added an h2 dependency and an integration test class for the marker controller

---
+++ project/map-backend/src/test/java/com/stevenst/app/controller/MarkerControllerIntegrationTest.java
@@ -0,0 +1,38 @@
+package com.stevenst.app.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class MarkerControllerIntegrationTest {
+	private static Server server;
+
+	@BeforeAll
+	void init() throws Exception {
+		server = Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+		server.start();
+	}
+
+	@AfterAll
+	void tearDown() throws SQLException {
+		server.stop();
+	}
+
+	@Test
+	void testTest() {
+		assertEquals(3, 1 + 2);
+	}
+}
