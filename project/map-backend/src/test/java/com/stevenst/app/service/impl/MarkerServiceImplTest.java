dc4b9eb5895c4e9857fb60b8d71902d16fd4355f
petreastefann <petreastefann@gmail.com>
2023-12-21 20:06:51 +0200

    added a test class for the marker service and a method to test getting all markers

---
+++ project/map-backend/src/test/java/com/stevenst/app/service/impl/MarkerServiceImplTest.java
@@ -0,0 +1,50 @@
+package com.stevenst.app.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.stevenst.app.model.Marker;
+import com.stevenst.app.repository.MarkerRepository;
+
+public class MarkerServiceImplTest {
+	@Mock
+	private MarkerRepository markerRepository;
+
+	@InjectMocks
+	private MarkerServiceImpl markerService;
+
+	@BeforeEach
+	public void setUp() {
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	void getAllMarkers() {
+		List<Marker> markers = Arrays.asList(
+				Marker.builder().id(-1L).title("Marker One").description("Description One").latitude(34.0522)
+						.longitude(-118.2437).startDate(LocalDateTime.now().plusDays(1))
+						.endDate(LocalDateTime.now().plusDays(2)).build(),
+				Marker.builder().id(-2L).title("Marker Two").description("Description Two").latitude(40.7128)
+						.longitude(-74.0060).startDate(LocalDateTime.now().plusDays(1))
+						.endDate(LocalDateTime.now().plusDays(2)).build(),
+				Marker.builder().id(-3L).title("Marker Three").description("Description Three").latitude(37.7749)
+						.longitude(-122.4194).startDate(LocalDateTime.now().plusDays(1))
+						.endDate(LocalDateTime.now().plusDays(2)).build());
+
+		when(markerRepository.findAll()).thenReturn(markers);
+
+		List<Marker> result = markerService.getAllMarkers();
+
+		assertEquals(markers, result);
+	}
+}
