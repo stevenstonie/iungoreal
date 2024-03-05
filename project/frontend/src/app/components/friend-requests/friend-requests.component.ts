40a710c0da0acfa275271fdd82f58ea04b7c2186
petreastefann <petreastefann@gmail.com>
2024-03-05 14:10:23 +0200

    (fr) added code in friend request component to get last 50 notifications

--- project/frontend/src/app/components/friend-requests/friend-requests.component.ts
+++ project/frontend/src/app/components/friend-requests/friend-requests.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NotificationFPayload } from 'src/app/models/payloads';
+import { NotificationsService } from 'src/app/services/notifications.service';
+import { map } from 'rxjs';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-friend-requests',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./friend-requests.component.scss']
 })
 export class FriendRequestsComponent {
+  last50NotificationsF: NotificationFPayload[] = [];
+  loggedUserUsername = localStorage.getItem('username') ?? '';
+  profilePictureUrl: string = 'assets/default-images/default-profile-picture.jpg';
 
+  constructor(private notificationsService: NotificationsService, private sanitizer: DomSanitizer) { }
+
+  ngOnInit(): void {
+    this.notificationsService.getLast50NotificationsF(this.loggedUserUsername).pipe(
+      map((notifications: NotificationFPayload[]) => {
+        return notifications.map(notification => {
+          if (notification.emitterPfpLink === '' || notification.emitterPfpLink === null) {
+            notification.emitterPfpLink = this.profilePictureUrl;
+          } else {
+            const sanitizedUrl: SafeUrl = this.sanitizer.bypassSecurityTrustUrl(notification.emitterPfpLink);
+          notification.emitterPfpLink = sanitizedUrl.toString();
+          }
+          return notification;
+        });
+      })
+    ).subscribe({
+      next: (notifications: NotificationFPayload[]) => {
+        this.last50NotificationsF = notifications;
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
+
+  // TODO!!!!!!!!: upodate the code so that the users who have no pfp will use the default one from the cloud
 }
--- project/frontend/src/app/components/notifications/notifications.component.ts
+++ project/frontend/src/app/components/notifications/notifications.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { NotificationsService } from '../../services/notifications.service';
-import { NotificationFPayload } from 'src/app/models/payloads';
 
 @Component({
   selector: 'app-notifications',
@@ -8,5 +6,5 @@ import { NotificationFPayload } from 'src/app/models/payloads';
   styleUrls: ['./notifications.component.scss']
 })
 export class NotificationsComponent {
-  
+
 }
