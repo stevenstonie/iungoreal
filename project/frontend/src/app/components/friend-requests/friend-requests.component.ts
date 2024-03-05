9e2215b8838fb6339e753a5e254ba78f93a570fb
petreastefann <petreastefann@gmail.com>
2024-03-05 15:19:59 +0200

    (fr) updated the code and styling for displaying the friends notifications + other small changes

--- project/frontend/src/app/components/friend-requests/friend-requests.component.ts
+++ project/frontend/src/app/components/friend-requests/friend-requests.component.ts
@@ -1,35 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NotificationFPayload } from 'src/app/models/payloads';
-import { NotificationsService } from 'src/app/services/notifications.service';
-import { map } from 'rxjs';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { NotificationService } from 'src/app/services/notification.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-friend-requests',
   templateUrl: './friend-requests.component.html',
   styleUrls: ['./friend-requests.component.scss']
 })
-export class FriendRequestsComponent {
+export class FriendRequestsComponent implements OnInit {
   last50NotificationsF: NotificationFPayload[] = [];
   loggedUserUsername = localStorage.getItem('username') ?? '';
   profilePictureUrl: string = 'assets/default-images/default-profile-picture.jpg';
 
-  constructor(private notificationsService: NotificationsService, private sanitizer: DomSanitizer) { }
+  constructor(private notificationService: NotificationService, private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
-    this.notificationsService.getLast50NotificationsF(this.loggedUserUsername).pipe(
-      map((notifications: NotificationFPayload[]) => {
-        return notifications.map(notification => {
-          if (notification.emitterPfpLink === '' || notification.emitterPfpLink === null) {
-            notification.emitterPfpLink = this.profilePictureUrl;
-          } else {
-            const sanitizedUrl: SafeUrl = this.sanitizer.bypassSecurityTrustUrl(notification.emitterPfpLink);
-          notification.emitterPfpLink = sanitizedUrl.toString();
-          }
-          return notification;
-        });
-      })
-    ).subscribe({
+    this.notificationService.getLast50NotificationsF(this.loggedUserUsername).subscribe({
       next: (notifications: NotificationFPayload[]) => {
         this.last50NotificationsF = notifications;
       },
@@ -38,6 +25,4 @@ export class FriendRequestsComponent {
       }
     });
   }
-
-  // TODO!!!!!!!!: upodate the code so that the users who have no pfp will use the default one from the cloud
 }
