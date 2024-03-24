4582a9f712f0e793d5a3c2949b42a4a9071f1325
petreastefann <petreastefann@gmail.com>
2024-03-24 19:54:05 +0200

    (bk (app)) moved the chat controller in the controller package and removed the old testing ChatMessage entity

--- project/app-service/src/main/java/com/stevenst/app/controller/ChatController.java
+++ project/app-service/src/main/java/com/stevenst/app/controller/ChatController.java
@@ -1,13 +1,15 @@
-package com.stevenst.app.test;
+package com.stevenst.app.controller;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.stevenst.app.model.ChatMessage;
+
 @RestController
 public class ChatController {
-	@MessageMapping("/hello")
-	@SendTo("/topic/greetings")
+	@MessageMapping("/chat.sendToChatroom")
+	@SendTo("/topic/chatroom/{chatroomId}")
 	public ChatMessage greeting(ChatMessage chatMessage) {
 		return chatMessage;
 	}
