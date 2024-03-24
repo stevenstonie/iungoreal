d2f1092cf663b233238c7eec3d5ebb88f07bc404
petreastefann <petreastefann@gmail.com>
2024-03-24 19:06:41 +0200

    (bk (app)) added Chatroom entity and ChatroomType enum

---
+++ project/app-service/src/main/java/com/stevenst/app/model/Chatroom.java
@@ -0,0 +1,39 @@
+package com.stevenst.app.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "\"chatroom\"")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Chatroom {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private ChatroomType type;
+
+	@Builder.Default
+	@Column(nullable = false)
+	private LocalDateTime lastMessageTime = LocalDateTime.now();
+}
---
+++ project/app-service/src/main/java/com/stevenst/app/model/ChatroomType.java
@@ -0,0 +1,5 @@
+package com.stevenst.app.model;
+
+public enum ChatroomType {
+	DM, GROUP, REGIONAL
+}
